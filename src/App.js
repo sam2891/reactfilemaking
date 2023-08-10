034cd7e6e7addeb517a3780b2cad472cdc5a040e
sam2891 <dwsnsamuel@gmail.com>
2023-08-10 18:47:44 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div classname="App">
+      <h3>My Portfolio</h3>
+
+      <h1>Samuel Dhandraj</h1>
+      <h2>Full Stack Developer</h2>
+      {
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+        </ul>
+      }
+      <p></p>
     </div>
   );
 }
